a41625d7071ce77470aafd0b72956754c9f1e37b
Dmytro Sharshakov <113136582+Picmaster911@users.noreply.github.com>
2023-10-14 18:09:31 +0300

    Add HW_04

---
+++ HomeWork_04/HomeWork_04/HomeWork_04/Point.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_04
+{
+    internal class Point
+    {
+        private int _x;
+        private int _y;
+        public static int CountInstanse;
+        /// <summary>
+        /// Input  X coordinate 0 - 2,147,483,647 
+        /// if input minus value automatic set default value = 0;
+        /// </summary>
+        public int X
+        {
+            get => _x;
+            set => _x = value < 0 ? 0 : value;
+        }
+        /// <summary>
+        /// <summary>
+        /// Input  Y coordinate 0 - 2,147,483,647 
+        /// if input minus value automatic set default value = 0;
+        /// </summary>
+        public int Y
+        {
+            get => _y;
+            set => _y = value < 0 ? 0 : value;
+        }
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+            CountInstanse += 1;
+        }
+        /// <summary>
+        /// Returns the distance between two objects ( input Obj type Point, and input Obj type Point )
+        /// </summary>
+        /// <param name="firstPoint"></param>
+        /// <param name="secondPoint"></param>
+        /// <returns></returns>
+        public static int GetDestancebetweenPoints(Point firstPoint, Point secondPoint)
+        {
+            var catet_1 = firstPoint.X > secondPoint.X ? firstPoint.X - secondPoint.X : secondPoint.X - firstPoint.X;
+            var catet_2 = firstPoint.Y > secondPoint.Y ? firstPoint.Y - secondPoint.Y : secondPoint.Y - firstPoint.Y;
+            return (int)Math.Round(Math.Sqrt(Math.Pow(catet_1, 2) + Math.Pow(catet_2, 2)));
+        }
+        /// <summary>
+        /// Returns the distance between two objects (this.Obj type Point and input Obj type Point )
+        /// </summary>
+        /// <param name="secondPoint"></param>
+        /// <returns></returns>
+        public int GetDistanceToPoint(Point secondPoint)
+        {
+            var catet_1 = _x > secondPoint.X ? _x - secondPoint.X : secondPoint.X - _x;
+            var catet_2 = _y > secondPoint.Y ? _y - secondPoint.Y : secondPoint.Y - _y;
+            return (int)Math.Round(Math.Sqrt(Math.Pow(catet_1, 2) + Math.Pow(catet_2, 2)));
+        }
+       
+    }
+}
+
---
+++ HomeWork_04/HomeWork_04/HomeWork_04/Program.cs
@@ -0,0 +1,8 @@
+﻿using HomeWork_04;
+
+var point1 = new Point(5, 5);
+var point2 = new Point(10, 10);
+
+Console.WriteLine (Point.GetDestancebetweenPoints(point2, point1));
+Console.WriteLine(point1.GetDistanceToPoint(point2));
+Console.WriteLine(Point.CountInstanse);
