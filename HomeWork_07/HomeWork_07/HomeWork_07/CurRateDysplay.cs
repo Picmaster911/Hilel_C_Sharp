31a3907656d0e0fb42f01f075d113425002b841d
LenovoPic <picmaster911@gmail.com>
2023-10-24 20:47:35 +0300

    HW07

---
+++ HomeWork_07/HomeWork_07/HomeWork_07/CurRateDysplay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_07
+{
+    public class CurRateDysplay<CurR> where CurR : struct
+    {
+        private CurR CurencyRate;
+        private List<CurR> CurencyRateList = new List<CurR>();
+
+        public void ChangeRate(CurR CurencyRate)
+        {
+            Console.WriteLine($"New Curency rate {CurencyRate}");
+            CurencyRateList.Add(CurencyRate);
+        }
+
+        public void PrintHistory()
+        {
+            foreach (var CurencyRate in CurencyRateList)
+            {
+                Console.WriteLine($"Curency rate history {CurencyRate}");
+            }
+        }
+    }
+}
---
+++ HomeWork_07/HomeWork_07/HomeWork_07/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HomeWork_07;
+
+var dysplayCurencyRate = new CurRateDysplay<double>();
+
+dysplayCurencyRate.ChangeRate(34.1);
+dysplayCurencyRate.ChangeRate(32.67);
+dysplayCurencyRate.ChangeRate(33);
+dysplayCurencyRate.PrintHistory();
