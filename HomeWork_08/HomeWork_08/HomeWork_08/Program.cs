17b0882da7a02f5d1dc10e886bb2f1c121935ca7
LenovoPic <picmaster911@gmail.com>
2023-10-27 15:29:23 +0300

    ChH08

--- HomeWork_08/HomeWork_08/HomeWork_08/Program.cs
+++ HomeWork_08/HomeWork_08/HomeWork_08/Program.cs
@@ -1,21 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-using HomeWork_08;
+﻿using HomeWork_08;
+using System;
 
 var person1 = new Person("Bob", 55, 15);
 var person2 = new Person("Mike", 60, 25);
 var person3 = new Person("Anna", 60, 10);
 Calculator calculPens;
+
 calculPens = Main.CalculatePensionForUK;
-person1.GetRetiremetnValue(calculPens);
-person2.GetRetiremetnValue(calculPens);
-person3.GetRetiremetnValue(calculPens);
+CalcAllPerson();
 calculPens = Main.CalculatePensionForUSA;
-person1.GetRetiremetnValue(calculPens);
-person2.GetRetiremetnValue(calculPens);
-person3.GetRetiremetnValue(calculPens);
+CalcAllPerson();
 calculPens = Main.CalculatePensionForAUS;
-person1.GetRetiremetnValue(calculPens);
-person2.GetRetiremetnValue(calculPens);
-person3.GetRetiremetnValue(calculPens);
+CalcAllPerson();
+
+void CalcAllPerson()
+{
+    person1.GetRetiremetnValue(calculPens);
+    person2.GetRetiremetnValue(calculPens);
+    person3.GetRetiremetnValue(calculPens);
+}
 
 public delegate double Calculator(int age, int experienve);
