471ea9d5193869e4af630ddc07f6bd223f141e09
LenovoPic <picmaster911@gmail.com>
2023-10-28 00:59:50 +0300

    CH8New

--- HomeWork_08/HomeWork_08/HomeWork_08/Main.cs
+++ HomeWork_08/HomeWork_08/HomeWork_08/Main.cs
@@ -8,65 +8,38 @@ namespace HomeWork_08
 {
     public class Main
     {
-        public static double CalculatePensionForUSA(int exp, int age)
+        public static double MatchMany(int allS, int age, double exp, double kCunt, string country)
         {
             if (exp <= 5)
             {
-                Console.WriteLine($"Pension USA = {(1000 / (65 - age) * (double)exp * 0.2) * 2.2}");
-                return (1000 / (65 - age) * (double)exp * 0.2) * 2.2;             
+                Console.WriteLine($"Pension {country} = {(allS / (65 - age) * (double)exp * 0.2) * kCunt}");
+                return (1000 / (65 - age) * (double)exp * 0.2) * 2.2;
             }
 
-               
+
             else if (exp > 5 && exp <= 10)
             {
-                Console.WriteLine($"Pension USA = {(1000 / (65 - age) * (double)exp * 0.6) * 2.2}");
+                Console.WriteLine($"Pension {country} = {(allS / (65 - age) * (double)exp * 0.6) * kCunt}");
                 return (1000 / (65 - age) * (double)exp * 0.6) * 2.2;
             }
             else
             {
-                Console.WriteLine($"Pension USA = {(1000 / (65 - age) * (double)exp * 1) * 2.2}");
+                Console.WriteLine($"Pension {country} = {(allS / (65 - age) * (double)exp * 1) * kCunt}");
                 return (1000 / (65 - age) * (double)exp * 1) * 2.2;
             }
         }
+        public static double CalculatePensionForUSA(int exp, int age)
+        {
+            return MatchMany(1000, age, exp, 2.2, "USA");
+        }
         public static double CalculatePensionForUK(int exp, int age)
         {
-            if (exp <= 5)
-            {
-                Console.WriteLine($"Pension UK = {(1000 / (65 - age) * (double)exp * 0.2) * 5.2}");
-                return (2000 / (65 - age) * (double)exp * 0.2) * 5.2;
-            }
-
-
-            else if (exp > 5 && exp <= 10)
-            {
-                Console.WriteLine($"Pension UK = {(1000 / (65 - age) * (double)exp * 0.6) * 5.2}");
-                return (2000 / (65 - age) * (double)exp * 0.6) * 5.2;
-            }
-            else
-            {
-                Console.WriteLine($"Pension UK = {(1000 / (65 - age) * (double)exp * 1) * 5.2}");
-                return (2000 / (65 - age) * (double)exp * 1) * 5.2;
-            }
+            return MatchMany(2000, age, exp, 5.2, "UK");
         }
         public static double CalculatePensionForAUS(int exp, int age)
         {
-            if (exp <= 5)
-            {
-                Console.WriteLine($"Pension AUS = {(1000 / (65 - age) * (double)exp * 0.2) * 3.2}");
-                return (3000 / (65 - age) * (double)exp * 0.2) * 3.2;
-            }
+            return MatchMany(3000, age, exp, 3.2, "AUS");
 
-
-            else if (exp > 5 && exp <= 10)
-            {
-                Console.WriteLine($"Pension AUS = {(1000 / (65 - age) * (double)exp * 0.6) * 3.2}");
-                return (3000 / (65 - age) * (double)exp * 0.6) * 3.2;
-            }
-            else
-            {
-                Console.WriteLine($"Pension AUS = {(1000 / (65 - age) * (double)exp * 1) * 3.2}");
-                return (3000 / (65 - age) * (double)exp * 1) * 3.2;
-            }
         }
     }
 }
