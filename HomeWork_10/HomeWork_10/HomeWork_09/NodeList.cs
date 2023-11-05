e1dd49808f5a505e309fe81a6a9bd24969ba9a63
LenovoPic <picmaster911@gmail.com>
2023-11-05 11:30:42 +0200

    HW10

---
+++ HomeWork_10/HomeWork_10/HomeWork_09/NodeList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_09
+{
+    internal class NodeList<T>
+    {
+        private Node<T> _firstNode;
+        private Node<T> _nowNode;
+        public void AddNode(T value)
+        {
+            if (_firstNode == null)
+            {
+                _firstNode = new Node<T>();
+                _firstNode.Value = value;
+            }
+            else
+            {
+                _nowNode = _firstNode;
+                while (_nowNode.NextNode != null)
+                {
+                    _nowNode = _nowNode.NextNode;
+                }
+                _nowNode.NextNode = new Node<T>();
+                _nowNode.NextNode.Value = value;
+            }
+        }
+        public void ShowAllNodes()
+        {
+            if (_firstNode != null)
+            {
+                _nowNode = _firstNode;
+                while (_nowNode != null)
+                {
+                    Console.WriteLine(_nowNode.Value);
+                    _nowNode = _nowNode.NextNode;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nodes is empty");
+            }
+        }
+        public void DeletNode(T value)
+        {
+            Node<T> nodePrev = null;
+            if (_firstNode != null)
+            {
+                _nowNode = _firstNode;
+                while (_nowNode != null && _nowNode.NextNode != null)
+                {
+                    if (_nowNode.Value.Equals(value))
+                    {
+                        nodePrev.NextNode = _nowNode.NextNode;
+                        _nowNode = _nowNode.NextNode;
+                    }
+                    else
+                    {
+                        nodePrev = _nowNode;
+                        _nowNode = _nowNode.NextNode;
+                    }
+                }
+            }
+        }
+        public void Clean ()
+        {
+            _firstNode = null;
+        }
+    }
+}
---
+++ HomeWork_10/HomeWork_10/HomeWork_09/Program.cs
@@ -0,0 +1,15 @@
+﻿using HomeWork_09;
+
+var list = new NodeList<string>();
+list.AddNode("1");
+list.AddNode("2");
+list.AddNode("3");
+list.AddNode("4");
+list.AddNode("5");
+list.ShowAllNodes();
+list.DeletNode("2");
+list.ShowAllNodes();
+list.Clean();
+list.ShowAllNodes();
+
+
