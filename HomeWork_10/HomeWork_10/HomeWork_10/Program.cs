51dc7355965ea1812bd0580faacbe00946bd2cbd
LenovoPic <picmaster911@gmail.com>
2023-11-05 12:00:12 +0200

    HW10CH

---
+++ HomeWork_10/HomeWork_10/HomeWork_10/Program.cs
@@ -0,0 +1,13 @@
+﻿using HomeWork_10;
+
+var list = new NodeList<string>();
+list.AddNode("1");
+list.AddNode("2");
+list.AddNode("3");
+list.AddNode("4");
+list.AddNode("5");
+list.ShowAllNodes();
+list.DeletNode("2");
+list.ShowAllNodes();
+list.Clean();
+list.ShowAllNodes();
