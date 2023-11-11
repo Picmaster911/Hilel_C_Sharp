3daeed427ba4a8379438b9f145027fd901935bf3
Dmytro Sharshakov <113136582+Picmaster911@users.noreply.github.com>
2023-11-11 09:58:16 +0200

    ChHW11

--- HomeWork_11/HomeWork_11/HomeWork_11/Program.cs
+++ HomeWork_11/HomeWork_11/HomeWork_11/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HomeWork_11;
 var newMath = new FactorialProcessor();
-newMath.Go(14, false);
-newMath.Go(14, true);
+newMath.Go(5, false);
+newMath.Go(10, false);
+newMath.Go(15, false);
+newMath.Go(5, true);
+newMath.Go(10, true);
+newMath.Go(15, true);
