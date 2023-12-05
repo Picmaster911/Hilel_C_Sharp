cf010d99d8fa50b8e7b4147497cbb98d437a5015
Dmytro Sharshakov <113136582+Picmaster911@users.noreply.github.com>
2023-12-05 18:57:40 +0200

    HW18

---
+++ HomeWork_18_Serialization/HomeWork_18_Serialization/HomeWork_18_Serialization/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_18_Serialization
+{
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        [NonSerialized]
+
+        public int Weight; 
+        public Person(string firstName, string lastName, int age, int weight)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Weight = weight;
+        }
+    }
+}
---
+++ HomeWork_18_Serialization/HomeWork_18_Serialization/HomeWork_18_Serialization/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HomeWork_18_Serialization;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+var person = new Person("volodymyr", "klychko", 55, 75);
+
+var personJson = JsonConvert.SerializeObject(person);
+
+BinaryFormatter formatter = new BinaryFormatter();
+byte[] serializeData;
+
+using (MemoryStream memoryStream = new MemoryStream())
+{
+    formatter.Serialize(memoryStream, personJson);
+    serializeData = memoryStream.ToArray();
+}
+
+using (MemoryStream memoryStream = new MemoryStream(serializeData))
+{
+    var jsonPerson = (string)formatter.Deserialize(memoryStream);
+    var objPerson = JsonConvert.DeserializeObject<Person>(jsonPerson);
+}
